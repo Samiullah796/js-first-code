112f57f1699d557b3f63cd9be88432052dec6231
Samiullah796 <samikhan2021karachi@gmail.com>
2024-08-30 12:40:34 +0500

    js first code

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Hello world")
+
+
+var a = prompt("Enter ur no")
+var isTrue = ("Are u sure u want to leave this page and ur computer")
+if (isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
